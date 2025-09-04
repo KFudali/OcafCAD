60384eb9eced06dd6e3feb3f6b23e4aded25dbe8
KFudali <krystian.fudali42@gmail.com>
2025-09-04 21:39:00 +0200

    Implement DocLabelUtils with simple unit tests

--- src/OcafKernel/DocumentDeltaObserver/PublisherDeltaObserver.cpp
+++ src/OcafKernel/DocumentDeltaObserver/PublisherDeltaObserver.cpp
@@ -1,4 +1,6 @@
 #include "PublisherDeltaObserver.hpp"
+#include "DocLabelUtils.hpp"
+
 
 PublisherDeltaObserver::PublisherDeltaObserver(
     AbstractOcafKernelPublisher& aPublisher
@@ -9,12 +11,20 @@ bool PublisherDeltaObserver::setDocument(Handle(TDocStd_Document) aDoc) {
     return false;
 };
 
-
 bool PublisherDeltaObserver::processDelta(Handle(TDF_Delta) aDelta) const {
     if (aDelta.IsNull()) return false;
     TDF_AttributeDeltaList attributeDeltas = aDelta->AttributeDeltas();
     for (auto attrDelta : attributeDeltas){
         TDF_Label label = attrDelta->Label();
+        if (DocLabelUtils::isRootAssemblyLabel(label)){
+            continue;
+        }
+        if (DocLabelUtils::isPrototypeLabel(label)){
+
+        } 
+        if (DocLabelUtils::isPartLabel(label)){
+
+        } 
     }
     return false;
 };
--- src/OcafKernel/PartDocument/PartDocTools/DocLabels/DocLabelUtils.cpp
+++ src/OcafKernel/PartDocument/PartDocTools/DocLabels/DocLabelUtils.cpp
@@ -1,13 +1,37 @@
 #include "DocLabelUtils.hpp"
 #include "RootAssemblyAttribute.hpp"
 
+#include "XCAFDoc_ShapeTool.hxx"
+#include "XCAFDoc_DocumentTool.hxx"
+
 bool DocLabelUtils::isRootAssemblyLabel(TDF_Label aLabel) {
     return aLabel.IsAttribute(RootAssemblyAttribute::GetID());
 }
 
 bool DocLabelUtils::isPartLabel(TDF_Label aLabel) {
-    return true;
+    if (aLabel.IsNull()){
+        return false;
+    }
+    auto comp = XCAFDoc_ShapeTool::IsComponent(aLabel);
+    auto shapeRef = XCAFDoc_ShapeTool::IsReference(aLabel);
+    if (comp && shapeRef){
+        return true;
+    }
+    return false;
 }
+
+
 bool DocLabelUtils::isPrototypeLabel(TDF_Label aLabel) {
-    return true;
+    if (aLabel.IsNull()){
+        return false;
+    }
+    auto shapeTool = XCAFDoc_DocumentTool::ShapeTool(aLabel.Root());
+    auto isTopLevel = shapeTool->IsTopLevel(aLabel);
+    auto isAssembly = shapeTool->IsAssembly(aLabel);
+    auto isRoot = DocLabelUtils::isRootAssemblyLabel(aLabel);
+    if ( isTopLevel && !isAssembly && !isRoot){
+        return true;
+    } else {
+        return false;
+    }
 }

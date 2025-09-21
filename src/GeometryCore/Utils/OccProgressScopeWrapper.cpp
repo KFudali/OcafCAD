fb34263239e3a5d1f6e55077fc6a4563a218b8b0
KFudali <krystian.fudali42@gmail.com>
2025-09-21 22:05:55 +0200

    Remove obsolete Utils dir from GeometryCore

--- src/GeometryCore/Utils/OccProgressScopeWrapper.cpp
+++
@@ -1,9 +0,0 @@
-#include "OccProgressScopeWrapper.hpp"
-
-
-void OccProgressScopeWrapper::Show(
-    const Message_ProgressScope& aScope, 
-    const Standard_Boolean force
-) {
-    mScope.launch('message');
-}

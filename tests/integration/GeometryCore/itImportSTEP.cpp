77b2392b6d08046d79688c8aa017a43be2e1d4a8
KFudali <krystian.fudali42@gmail.com>
2025-09-30 21:48:55 +0200

    Extend basic STEPimport integration tests

--- tests/integration/GeometryCore/itImportSTEP.cpp
+++ tests/integration/GeometryCore/itImportSTEP.cpp
@@ -36,4 +36,53 @@ TEST_F(GeometryCoreImportSTEPIntegrationTest, TestImportCube) {
 
     core->import(importer);
     ASSERT_EQ(core->view()->topLevelPartLabels().size(), 1);
+    ASSERT_EQ(core->view()->topLevelParts().size(), 1);
+    ASSERT_EQ(core->view()->geometryObjects().size(), 1);
+}
+
+TEST_F(GeometryCoreImportSTEPIntegrationTest, TestUndoImportCube) {
+    using ::testing::_;
+    using ::testing::Exactly;
+
+    auto mockObserver = std::make_unique<MockGeometryObserver>();
+
+    EXPECT_CALL(*mockObserver, onPartAdded(_)).Times(Exactly(1));
+    EXPECT_CALL(*mockObserver, onPartRemoved(_)).Times(Exactly(1));
+    
+    core->events()->appendObserver(mockObserver.get());
+
+    std::shared_ptr<AbstractInputSource> source =
+        std::make_shared<FileSource>(StubSTEPFileSources::singlePartCube());
+    STEPImporter importer(source);
+
+    core->import(importer);
+    core->commands().undo();
+
+    ASSERT_EQ(core->view()->topLevelPartLabels().size(), 0);
+    ASSERT_EQ(core->view()->topLevelParts().size(), 0);
+    ASSERT_EQ(core->view()->geometryObjects().size(), 0);
+}
+
+TEST_F(GeometryCoreImportSTEPIntegrationTest, TestRedoImportCube) {
+    using ::testing::_;
+    using ::testing::Exactly;
+
+    auto mockObserver = std::make_unique<MockGeometryObserver>();
+
+    EXPECT_CALL(*mockObserver, onPartAdded(_)).Times(Exactly(2));
+    EXPECT_CALL(*mockObserver, onPartRemoved(_)).Times(Exactly(1));
+    
+    core->events()->appendObserver(mockObserver.get());
+
+    std::shared_ptr<AbstractInputSource> source =
+        std::make_shared<FileSource>(StubSTEPFileSources::singlePartCube());
+    STEPImporter importer(source);
+
+    core->import(importer);
+    core->commands().undo();
+    core->commands().redo();
+
+    ASSERT_EQ(core->view()->topLevelPartLabels().size(), 1);
+    ASSERT_EQ(core->view()->topLevelParts().size(), 1);
+    ASSERT_EQ(core->view()->geometryObjects().size(), 1);
 }
